0fd8b7b9d7ef71d76ef7fff4f27754c8fcccf53c
xtly2012 <xtly2012@163.com>
2016-10-30 02:24:28 +0800

    parser

---
+++ codeTest/src/main/java/com/chen/compile/Parser.java
@@ -0,0 +1,52 @@
+package com.chen.compile;
+
+import java.io.IOException;
+
+public class Parser {
+	static int lookahead;
+	
+	public Parser() throws IOException {
+		lookahead = System.in.read();
+	}
+	
+	void expr() throws IOException {
+		term();
+		while(true) {
+			if (lookahead == '+') {
+				match('+');
+				term();
+				System.out.write('+');
+			} else if (lookahead == '-') {
+				match('-');
+				term();
+				System.out.write('-');
+			} else {
+				return;
+			}
+		}
+	}
+	
+	void term() throws IOException {
+		if (Character.isDigit((char)lookahead)) {
+//			System.out.write((char)lookahead);
+			match(lookahead);
+		} else { 
+			throw new Error("syntax error");
+		}
+	}
+	
+	void match(int t) throws IOException {
+		if (lookahead == t) {
+			lookahead = System.in.read();
+		} else {
+			throw new Error("syntax error");
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Parser parser = new Parser();
+		parser.expr();
+		System.out.write('\n');
+	}
+	
+}
